703d544188d8d022fe0b0d371330bfaa2f85daf4
nhuyle287 <50352152+nhuyle287@users.noreply.github.com>
2020-07-16 10:57:12 +0700

    update/layout

--- app/src/main/java/com/foodorderingapp/activity/client/mefragment/MeFragement.java
+++ app/src/main/java/com/foodorderingapp/activity/client/mefragment/MeFragement.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.foodorderingapp.R;
+import com.foodorderingapp.activity.LoginActivity;
 import com.foodorderingapp.model.KhachHang;
 import com.foodorderingapp.model.TaiKhoan;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -128,7 +129,16 @@ public class MeFragement extends Fragment {
                     }
                 });
 
+        Button buttondong=(Button) view.findViewById(R.id.dangxuatbutton);
+        buttondong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iSubsua = new Intent(getContext(), LoginActivity.class);
 
+
+                startActivity(iSubsua);
+
+            }});
 
 
     }
